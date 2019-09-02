e892cdf7585b31cd8a0e8d57928c31a9e5c2bfb1
Cody Dupuis <ctdupuis7@gmail.com>
2019-09-02 01:36:17 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,34 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  #Check if name is ALREADY added. Yes: do nothing. No: add 
+  hash = {}
+  data.each do |colors_gender_lives, values|
+    values.each do |value, array|
+      array.each do |name|
+        if hash[name] == nil
+          hash[name] = {}
+            hash[name][colors_gender_lives] = []
+        else
+          hash[name][colors_gender_lives] = []
+        end
+      end
+    end
+  end
+  hash.each do |name, values|
+    values.each do |hashvalue, array|
+      data.each do |colors_gender_lives, values|
+        values.each do |value, array|
+          
+          array.each do |element|
+            if element == name && hashvalue == colors_gender_lives
+              
+              hash[name][hashvalue] << value.to_s
+            end
+          end
+        end
+      end
+    end
+  end
+  hash
 end
