1d49fdf0e4188bc6ce4d302bf4afe090e54e21e0
Cody Dupuis <ctdupuis7@gmail.com>
2019-09-04 14:45:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,34 +1,29 @@
-require 'pry'
-
 def nyc_pigeon_organizer(data)
-  #Check if name is ALREADY added. Yes: do nothing. No: add 
-  hash = {}
-  data.each do |colors_gender_lives, values|
-    values.each do |value, array|
-      array.each do |name|
-        if hash[name] == nil
-          hash[name] = {}
-            hash[name][colors_gender_lives] = []
-        else
-          hash[name][colors_gender_lives] = []
-        end
+hash = {}
+data.each do |colors_gender_lives, values|
+  values.each do |value, array|
+    array.each do |name|
+      if hash[name] == nil 
+        hash[name] = {}
+        hash[name][colors_gender_lives] = []
+      else
+        hash[name][colors_gender_lives] = []
       end
     end
   end
-  hash.each do |name, values|
-    values.each do |hashvalue, array|
-      data.each do |colors_gender_lives, values|
-        values.each do |value, array|
-          
-          array.each do |element|
-            if element == name && hashvalue == colors_gender_lives
-              
-              hash[name][hashvalue] << value.to_s
-            end
+end
+hash.each do |name, values|
+  values.each do |hashvalue, array|
+    data.each do |color_gender_lives, values|
+      values.each do |value, array|
+        array.each do |element|
+          if element == name && hashvalue == color_gender_lives
+            hash[name][hashvalue] << value.to_s 
           end
         end
       end
     end
   end
-  hash
+end
+hash
 end
